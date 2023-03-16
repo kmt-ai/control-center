128b0cb97b5270d27e5231f02e9eff01334e5b19
Paladin581 <shulga.aleks.v@gmail.com>
2023-03-16 20:39:21 +0300

    papers

--- 10TControlCenter/10TControlCenter/Form1.cs
+++ 10TControlCenter/10TControlCenter/Form1.cs
@@ -1,19 +1,41 @@
 using System.IO;
+using System.Windows.Forms;
+
 namespace _10TControlCenter
 {
     public partial class Form1 : Form
     {
-        string path = @"C:\Users\Александр\Desktop\test\Team1.txt";
+        string bname = "";
+        string path = @"C:\Users\Александр\Desktop\test\";
+        string[] timInfo = {""};
+        void teamPic(string bname, string path)
+        {
+            path = path + bname + ".txt";
+            StreamReader st = new StreamReader(path);
+            timInfo[0] = st.ReadToEnd();
+            listBox1.Items.AddRange(timInfo);
+            st.Close();
+        }
         public Form1()
         {   
             InitializeComponent();
-            StreamReader st = new StreamReader(path);
-            
-           // button1.Text= st.ReadLine();
             
             
+           
+         
+
         }
 
-        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bname = button1.Text;
+            teamPic(bname, path);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            bname = button2.Text;
+            teamPic(bname, path);
+        }
     }
 }
